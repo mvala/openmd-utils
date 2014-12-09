e3d255bf13e16f20a623e8195ae4db434418514c
Martin Vala <mvala@saske.sk>
2014-12-09 11:44:00 +0100

    Init commit

---
+++ cmake/modules/FindOPENMD.cmake
@@ -0,0 +1,25 @@
+# - Try to find OPENMD
+# Once done this will define
+#  OPENMD_FOUND - System has OPENMD
+#  OPENMD_INCLUDE_DIRS - The OPENMD include directories
+#  OPENMD_LIBRARIES - The libraries needed to use OPENMD
+
+
+find_path(OPENMD_INCLUDE_DIR DumpReader.hpp
+          HINTS /usr/include/openmd
+          PATH_SUFFIXES OPENMD )
+
+find_library(OPENMD_LIBRARY NAMES openmd_core OPENMD
+             HINTS /usr/lib64 )
+
+set(OPENMD_LIBRARIES ${OPENMD_LIBRARY} )
+set(OPENMD_INCLUDE_DIRS ${OPENMD_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set OPENMD_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(OPENMD  DEFAULT_MSG
+                                  OPENMD_LIBRARY OPENMD_INCLUDE_DIR)
+
+mark_as_advanced(OPENMD_INCLUDE_DIR OPENMD_LIBRARY )
+
